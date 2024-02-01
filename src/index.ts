37a4ac7a8e9d2f5e21a176dd3000715b95ec3e3c
Jack Garritano <114269804+jackgarritano@users.noreply.github.com>
2024-02-01 10:24:42 -0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express, { type Express, type Request, type Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import { exampleRoute } from "./routes/exampleRoute";
+import { verifyToken } from "./middlewares/verifyToken";
+import { notFound, errorHandler } from "./middlewares/errors";
+
+dotenv.config();
+
+const app: Express = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors()); // Allow cross-origin requests (for frontend to communicate with backend on different ports/address)
+app.use(express.json()); // Parses incoming JSON requests and uts the parsed data in req
+app.use(express.urlencoded({ extended: true })); // Parses incoming requests with urlenconded payloads
+// error handling and better logging
+app.use(morgan("dev"));
+app.use(helmet());
+
+/**
+ * Uses the verifyToken middleware to protect the "/data" route
+ * Use the verifyToken to protect all the routes that require authentication
+ */
+app.use("/example", verifyToken, exampleRoute);
+
+// Default route: Unprotected
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Express + Typescript Auth Server Temp!");
+});
+
+// error handling route
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
