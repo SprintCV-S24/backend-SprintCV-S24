c82bd613672d51584b98280d9e49756020664a19
Jack Garritano <114269804+jackgarritano@users.noreply.github.com>
2024-02-25 13:38:54 -0600

    adds tests

--- src/tests/controllers.tests/activities.test.ts
+++ src/tests/controllers.tests/activities.test.ts
@@ -11,7 +11,22 @@ describe("Activities controller tests", () => {
 	test("Adds and retrieves an activity", async () => {
 		await createActivity(activityDummyData1 as ActivitiesType);
 		const returnedActivities = await getAllActivities(activityDummyData1.user);
+
+		//get back the 1 activity that was added
 		expect(returnedActivities.length).to.equal(1);
 		expect(returnedActivities[0]).toMatchObject(activityDummyData1);
+
+		//Can't add duplicate name
+		await expect(createActivity(activityDummyData1 as ActivitiesType)).rejects.toThrowError();
+
+		const returnedActivities2 = await getAllActivities(activityDummyData1.user);
+
+		//if duplicate, shouldn't add to db
+		expect(returnedActivities2.length).to.equal(1);
+
+		const returnedActivities3 = await getAllActivities("fakeuserid");
+
+		//don't get records for a different user id
+		expect(returnedActivities3.length).to.equal(0);
 	})
 })
