293275d371a327067f67c0b70d38327f569282bb
seancrich11 <sean.c.richardson@vanderbilt.edu>
2024-02-29 19:26:54 -0600

    Fix root.router

--- src/routers/root.router.ts
+++ src/routers/root.router.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { skillsRouter } from "./skills.router";
+import { skillRouter } from "./skills.router";
 import { activitiesRouter } from "./activities.router";
 import { educationRouter } from "./education.router";
 
 export const router = Router();
 
-router.use("/skills", skillsRouter);
+router.use("/skills", skillRouter);
 router.use("/activities", activitiesRouter);
 router.use("/education", educationRouter);
