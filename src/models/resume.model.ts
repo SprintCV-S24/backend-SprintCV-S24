b9bd321cb398d9dd161bde569bd61b42acf3c43c
Jack Garritano <114269804+jackgarritano@users.noreply.github.com>
2024-04-15 20:56:09 -0500

    templates support

--- src/models/resume.model.ts
+++ src/models/resume.model.ts
@@ -3,19 +3,24 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
+export enum templates {
+  JAKES = "jakes",
+  BLUE = "blue",
+}
+
 //typescript type corresponding with the mongoose schema structure
 export interface resumeType extends mongoose.Document {
-	user: string;
-	itemName: string;
+  user: string;
+  itemName: string;
   itemIds: mongoose.Types.ObjectId[];
-  templateId: mongoose.Types.ObjectId;
+  templateId: templates;
 }
 
 const Resume = new Schema<resumeType>({
-	user: { type: String, required: true },
-	itemName: { type: String, required: true },
+  user: { type: String, required: true },
+  itemName: { type: String, required: true },
   itemIds: { type: [Schema.Types.ObjectId], required: true },
-  templateId: { type: Schema.Types.ObjectId, required: false, default: null },
+  templateId: { type: String, enum: Object.values(templates), required: true, default: templates.JAKES },
 });
 
 export const ResumeModel = mongoose.model("Resume", Resume);
